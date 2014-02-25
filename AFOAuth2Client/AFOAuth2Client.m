1f5eeb83cf2974d4f2b60b8720e1367bd183f253
Thibaut Jarosz <thibaut.jarosz@gmail.com>
2014-02-25 14:59:39 +0100

    Fix compatibility with OS X prior to 10.9

--- AFOAuth2Client/AFOAuth2Client.m
+++ AFOAuth2Client/AFOAuth2Client.m
@@ -281,7 +281,8 @@ static NSMutableDictionary * AFKeychainQueryDictionaryWithIdentifierAndServiceNa
 {
     id securityAccessibility = nil;
 #if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 43000) || (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1090)
-    securityAccessibility = (__bridge id)kSecAttrAccessibleWhenUnlocked;
+    if ( &kSecAttrAccessibleWhenUnlocked != NULL )
+        securityAccessibility = kSecAttrAccessibleWhenUnlocked;
 #endif
     
     return [[self class] storeCredential:credential withIdentifier:identifier withAccessibility:securityAccessibility withServiceName:serviceName];
